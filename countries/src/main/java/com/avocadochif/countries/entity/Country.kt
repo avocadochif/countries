528ae0b707842c6d9754acec8fbf773347176037
avocadochif <avocadochif@gmail.com>
2023-05-07 23:08:36 +0300

    Added base entity for library.

---
+++ countries/src/main/java/com/avocadochif/countries/entity/Country.kt
@@ -0,0 +1,8 @@
+package com.avocadochif.countries.entity
+
+data class Country(
+    val id: String,     // ISO-3166-1 alpha-3
+    val name: String,   // ISO-3166-1
+    val flag: String,   // unicode emoji
+    val phone: String   // E.164
+)
